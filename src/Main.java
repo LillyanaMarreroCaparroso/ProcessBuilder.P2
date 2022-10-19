fdad908322d989f7b4dcc7306d527ef3e373f877
mac <mac@MacBook-Air-de-mac.local>
2022-10-19 13:27:18 +0100

    Feat: Creado objeto ProcessBuilder y objeto arrayList

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,9 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
 
+        ProcessBuilder pb = new ProcessBuilder("open", "-a", "Firefox");
+        Process myProcess = null;
+        ArrayList <Process> browsers = new ArrayList<>();
 
 
 
